285d8f0cafcb395507d8a1f5f51070779815573b
Ron Arbel <ronarbel1@gmail.com>
2019-01-27 17:17:52 -0800

    play first sound

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import Sound from 'react-sound';
 import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
+    this.state = {
+      playStatus: Sound.status.STOPPED,
+    };
+
+    this.playSound = this.playSound.bind(this);
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+    // $.ajax({
+    //   url: '/items',
+    //   success: (data) => {
+    //     this.setState({
+    //       items: data,
+    //     });
+    //   },
+    //   error: (err) => {
+    //     console.log('err', err);
+    //   },
+    // });
+
+  }
+  
+  playSound() {
+    this.setState({ playStatus: Sound.status.PLAYING });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    const { playStatus } = this.state;
+    return (
+      <div>
+        <h1>Item List</h1>
+        {/* <List items={this.state.items} /> */}
+        <button type="button" onClick={this.playSound}> Play </button>
+        <Sound
+          url="https://www.madeon.fr/adventuremachine/wmas/assets/audio/drum.1.6.ogg?1427136627194"
+          playStatus={playStatus}
+          onFinishedPlaying={this.playSound}
+          // playFromPosition={300 /* in milliseconds */}
+          // onLoading={this.handleSongLoading}
+          // onPlaying={this.handleSongPlaying}
+          // onFinishedPlaying={this.handleSongFinishedPlaying}
+        />
+      </div>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,29 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = 'path';
 
-var app = express();
+const app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(`${__dirname}/../react-client/dist`));
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+app.get('/sounds', (req, res) => {
+  res.sendFile(path.join(__dirname, '../samples/drums/', 'drum.1.6.gg'));
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
-
