59cbf529ecdbaa99b71dcb4d1fc6f0929f49ac62
Ron Arbel <ronarbel1@gmail.com>
2019-01-27 19:56:22 -0800

    refactor state

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,8 +6,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      drum1Active: false,
-      drum1PlayStatus: Sound.status.STOPPED,
+      drum1Status: 'inactive',
+      drum2Status: 'inactive',
     };
 
     this.playSounds = this.playSounds.bind(this);
@@ -34,16 +34,22 @@ class App extends React.Component {
     this.setState(newState);
   }
 
-  toggleSound(sound) {
-    console.log(sound);
-    this.setState({ [sound]: !this.state[sound] });
+  toggleSongClipStatus(soundClip) {
+    const soundClipStatusKey = soundClip += 'Status'
+    const oldSoundClipStatus = this.state[soundClipStatusKey];
+
+    if (oldSoundClipStatus === 'inactive') {
+      this.setState({})
+    }
   }
+
+  queued 
 
   render() {
     const { drum1PlayStatus } = this.state;
     return (
       <div>
-        <button type="button" value="drum1Active" onClick={e => this.toggleSound(e.target.value)}> Drum1 </button>
+        <button type="button" value="drum1" onClick={e => this.toggleSampleStatus(e.target.value)}> Drum1 </button>
         <Sound
           url="https://www.madeon.fr/adventuremachine/wmas/assets/audio/drum.1.6.ogg?1427136627194"
           playStatus={drum1PlayStatus}
