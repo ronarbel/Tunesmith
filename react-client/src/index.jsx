c72021bdb57f4c8ce2e74d9183c1b81f02fd3695
Ron Arbel <ronarbel1@gmail.com>
2019-01-27 18:54:39 -0800

    experiment with toggle launch

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,53 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import Sound from 'react-sound';
-import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playStatus: Sound.status.STOPPED,
+      drum1Active: false,
+      drum1PlayStatus: Sound.status.STOPPED,
     };
 
-    this.playSound = this.playSound.bind(this);
+    this.playSounds = this.playSounds.bind(this);
+    this.toggleSound = this.toggleSound.bind(this);
   }
 
   componentDidMount() {
-    // $.ajax({
-    //   url: '/items',
-    //   success: (data) => {
-    //     this.setState({
-    //       items: data,
-    //     });
-    //   },
-    //   error: (err) => {
-    //     console.log('err', err);
-    //   },
-    // });
-
+    this.interval = setInterval(this.playSounds, 4363.628000000001);
   }
-  
-  playSound() {
-    this.setState({ playStatus: Sound.status.PLAYING });
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  playSounds() {
+    const newState = {};
+    const {
+      drum1Active,
+    } = this.state;
+
+    if (drum1Active) {
+      newState.drum1PlayStatus = Sound.status.PLAYING;
+    }
+    this.setState(newState);
+  }
+
+  toggleSound(sound) {
+    console.log(sound);
+    this.setState({ [sound]: !this.state[sound] });
   }
 
   render() {
-    const { playStatus } = this.state;
+    const { drum1PlayStatus } = this.state;
     return (
       <div>
-        <h1>Item List</h1>
-        {/* <List items={this.state.items} /> */}
-        <button type="button" onClick={this.playSound}> Play </button>
+        <button type="button" value="drum1Active" onClick={e => this.toggleSound(e.target.value)}> Drum1 </button>
         <Sound
           url="https://www.madeon.fr/adventuremachine/wmas/assets/audio/drum.1.6.ogg?1427136627194"
-          playStatus={playStatus}
-          onFinishedPlaying={this.playSound}
-          // playFromPosition={300 /* in milliseconds */}
-          // onLoading={this.handleSongLoading}
-          // onPlaying={this.handleSongPlaying}
-          // onFinishedPlaying={this.handleSongFinishedPlaying}
+          playStatus={drum1PlayStatus}
+          playFromPosition={0}
         />
       </div>
     );
